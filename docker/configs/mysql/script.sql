fa6cd8d53f29f98aa84f490d799a74e0bcfbe099
Emanuel Cabrera Zembrunski <emanuel.zembrunski@gmail.com>
2022-11-20 17:23:41 -0300

    modificacion en el script de la base de datos

--- docker/configs/mysql/script.sql
+++ docker/configs/mysql/script.sql
@@ -1,3 +1,3 @@
 use devops_app;
 create table welcome (id int not null AUTO_INCREMENT, description varchar(255) not null, PRIMARY KEY (id));
-insert into welcome (description) values ('Curso devOps'),('UTN-BA');
+insert into welcome (description) values ('Curso devOps'),('UTN-BA'),('Grupo nro 6');
