71fc1447faeb657d6394d3d7e30e41aecb8a8d5f
Emanuel Cabrera Zembrunski <emanuel.zembrunski@gmail.com>
2022-12-11 10:34:53 -0300

    tp 4

--- Vagrant.bootstrap.sh
+++ Vagrant.bootstrap.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-#Actualizo los paquetes de la maquina virtual
-sudo apt-get update -y
-
-#Aprovisionamiento de software
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common linux-image-extra-virtual-hwe-$(lsb_release -r |awk  '{ print $2 }') linux-image-extra-virtual
-
-# Muevo el archivo de configuración de firewall al lugar correspondiente
-if [ -f "/tmp/ufw" ]; then
-	sudo mv -f /tmp/ufw /etc/default/ufw
-fi
-
-# Muevo el archivo hosts. En este archivo esta asociado el nombre de dominio con una dirección
-# ip para que funcione las configuraciones de Puppet
-if [ -f "/tmp/etc_hosts.txt" ]; then
-	sudo mv -f /tmp/etc_hosts.txt /etc/hosts
+##Genero una partición swap. Previene errores de falta de memoria
+if [ ! -f "/swapdir/swapfile" ]; then
+	sudo mkdir /swapdir
+	cd /swapdir
+	sudo dd if=/dev/zero of=/swapdir/swapfile bs=1024 count=2000000
+  sudo chmod 600 /swapdir/swapfile
+	sudo mkswap -f  /swapdir/swapfile
+	sudo swapon swapfile
+	echo "/swapdir/swapfile       none    swap    sw      0       0" | sudo tee -a /etc/fstab /etc/fstab
+	sudo sysctl vm.swappiness=10
+	echo vm.swappiness = 10 | sudo tee -a /etc/sysctl.conf
 fi
 
 #### Instalacion de JDK#####
 sudo apt-get install apt openjdk-11-jdk -y
 
-###### Instalación de Puppet ######
+###### Puppet ######
 #Directorios
 PUPPET_DIR="/etc/puppet"
 ENVIRONMENT_DIR="${PUPPET_DIR}/code/environments/production"
 PUPPET_MODULES="${ENVIRONMENT_DIR}/modules"
+
 if [ ! -x "$(command -v puppet)" ]; then
-  #configuración de repositorio
+
+	#### Instalacion puppet master
+  #Directorios
+
 	sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
  	sudo apt-get update
 	sudo apt install -y puppetmaster
@@ -58,16 +58,32 @@ if [ ! -x "$(command -v puppet)" ]; then
   # Estructura de directorios para crear el modulo de Jenkins
   sudo mkdir -p $PUPPET_MODULES/jenkins/{manifests,files}
 
-  # muevo los archivos que utiliza Puppet
-  sudo mv -f /tmp/site.pp $ENVIRONMENT_DIR/manifests #/etc/puppet/manifests/
-  sudo mv -f /tmp/init.pp $PUPPET_MODULES/docker_install/manifests/init.pp
-  sudo mv -f /tmp/env $PUPPET_MODULES/docker_install/files
-  sudo mv -f /tmp/init_jenkins.pp $PUPPET_MODULES/jenkins/manifests/init.pp
   sudo cp /usr/share/doc/puppet/examples/etckeeper-integration/*commit* $PUPPET_DIR
   sudo chmod 755 $PUPPET_DIR/etckeeper-commit-p*
+
+fi
+
+# muevo los archivos que utiliza Puppet
+if [ -f "/tmp/site.pp" ]; then
+  sudo cp -f /tmp/site.pp $ENVIRONMENT_DIR/manifests
+fi
+
+if [ -f "/tmp/init.pp" ]; then
+  sudo cp -f /tmp/init.pp $PUPPET_MODULES/docker_install/manifests/init.pp
+fi
+
+if [ -f "/tmp/env" ]; then
+  sudo cp -f /tmp/env $PUPPET_MODULES/docker_install/files
+fi
+if [ -f "/tmp/init_jenkins.pp" ]; then
+  sudo cp -f /tmp/init_jenkins.pp $PUPPET_MODULES/jenkins/manifests/init.pp
+fi
+if [ -f "/tmp/jenkins_dependencies.pp" ]; then
+  sudo cp -f /tmp/jenkins_dependencies.pp $PUPPET_MODULES/jenkins/manifests/dependencies.pp
 fi
 
 
+#Habilito el puerto en el firewall
 sudo ufw allow 8140/tcp
 
 # al detener e iniciar el servicio se regeneran los certificados
@@ -75,6 +91,8 @@ echo "Reiniciando servicios puppetmaster y puppet agent"
 sudo systemctl stop puppetmaster && sudo systemctl start puppetmaster
 sudo systemctl stop puppet && sudo systemctl start puppet
 
+echo "Instalacion modulo sudo"
+sudo puppet module install saz-sudo
 
 # limpieza de configuración del dominio utn-devops.localhost es nuestro nodo agente.
 # en nuestro caso es la misma máquina
@@ -82,4 +100,3 @@ sudo puppet node clean utn-devops.localhost
 
 # Habilito el agente
 sudo puppet agent --certname utn-devops.localhost --enable
-
--- hostConfigs/puppet/init_jenkins.pp
+++ hostConfigs/puppet/init_jenkins.pp
@@ -1,41 +1,55 @@
+#
 class jenkins {
 
     # get key
     exec { 'install_jenkins_key':
-        command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -',
+        command => '/usr/bin/wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -',
     }
 
-        # source file
+    # source file
     file { '/etc/apt/sources.list.d/jenkins.list':
+        ensure  => present,
         content => "deb https://pkg.jenkins.io/debian-stable binary/\n",
-        ensure => present,
         mode    => '0644',
         owner   => root,
         group   => root,
         require => Exec['install_jenkins_key'],
-    } -> #ordeno la secuencia de pasos en el tiempo mediante el operador "->".
-    # se utiliza para encadenar semanticamente distintas declaraciones
+    }
+
     # update
     exec { 'apt-get update':
         command => '/usr/bin/apt-get update',
     }
 
     #install jenkins
-    $enhancers = [ 'openjdk-11-jre', 'jenkins' ]
+    $enhancers = [ 'openjdk-8-jre', 'jenkins' ]
 
+    # jenkins package
     package { $enhancers:
-        ensure => 'installed',
-    } ->
-    #Reemplazo el puerto de jenkins para que este escuchando en el 8082
-    exec { 'replace_jenkins_port':
+        ensure  => installed,
+        require => [
+            File['/etc/apt/sources.list.d/jenkins.list'],
+            Exec['apt-get update']
+        ]
+    } -> exec { 'replace_jenkins_port': #Reemplazo el puerto de jenkins para que este escuchando en el 8082
         command => "/bin/sed -i -- 's/JENKINS_PORT=8080/JENKINS_PORT=8082/g' /lib/systemd/system/jenkins.service",
-    } -> exec { 'reload-systemctl':
-        command => '/bin/systemctl daemon-reload',
-        notify  => Service['jenkins'],
+
+    } -> exec { 'reload': # Notifico al gestor de servicios que un archivo cambio
+        command => '/bin/systemctl restart jenkins',
+        path    => '/usr/bin:/usr/sbin:/bin',
+        unless  => 'netstat -pant |grep LISTEN |grep 8082 | awk  \'{print $7}\'',
     }
 
-    # aseguro que el servicio de jenkins este activo
     service { 'jenkins':
-        ensure  => running,
+        ensure => running,
+        enable => true,
     }
+
+    user { 'jenkins':
+        ensure   => present,
+        password => '$1$hrl1RNSP$DoKnhDdeCLlW.QJGLY8dj1',
+        home     => '/var/lib/jenkins',
+        shell    => '/bin/bash',
+    }
+
 }
